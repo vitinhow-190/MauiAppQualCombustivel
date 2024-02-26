21419cc878b61a9529ecd728b9f7b6ba96b96470
Usuario <Usuario@CPD03-M16>
2024-02-26 14:06:07 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppQualCombustivel/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppQualCombustivel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppQualCombustivel/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppQualCombustivel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
